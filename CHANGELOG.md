b3fda3a2e8d5dc8f9e3741ae68ee1ded2900d583
Gennadii Zakharov <Gennadiy.Zakharov@gmail.com>
2025-07-13 11:09:48 +0100

    Mocked one-step workflow

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# winequalitywf changelog
+
+## 0.0.1 (2025-07-13)
---
+++ src/package.ts
@@ -0,0 +1,39 @@
+/* Do not change these import lines to match external modules in webpack configuration */
+import * as grok from 'datagrok-api/grok';
+import * as ui from 'datagrok-api/ui';
+import * as DG from 'datagrok-api/dg';
+import type { PipelineConfiguration } from '@datagrok-libraries/compute-api';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export const _package = new DG.Package();
+
+//tags: model
+//editor: Compute2:TreeWizardEditor
+//input: object params
+//output: object result
+export async function WineQualityWf(params: any) {
+  const c: PipelineConfiguration = {
+    id: 'winequalitywf',
+    nqName: 'winequalitywf:WineQualityWf',
+    version: '1.0',
+    type: 'static',
+    steps: [{
+      id: 'step1',
+      nqName: 'winequalitywf:MyAddScript',
+    }]
+  };
+  return c;
+}
+
+//input: double a
+//input: double b
+//output: double c
+export function MyAddScript(a: number, b: number) {
+  return a + b;
+}
+
